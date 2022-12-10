3e9533d2ed8b08b582fdafac9dc5224bd1000942
mateuszz_wsb <mateuszo.85@gmail.com>
2022-12-10 13:00:45 +0100

    message

--- create.py
+++ create.py
@@ -1,5 +1,8 @@
 import os
 import time
 
-os.system('cmd /c "cd c://Users//vdi-student//Desktop//nauka//first-repo && dir /s C://Users//vdi-student//Desktop//nauka//first-repo//new2.txt >> result.txt"')
-time.sleep(15)
+os.system('cmd \c "cd c://Users//vdi-student//Desktop//nauka//first-repo"') 
+time.sleep(2)
+os.system('cmd \c "dir \s C://Users//vdi-student//Desktop//nauka//first-repo//new2.txt >> result.txt"')
+time.sleep(2)
+
---
+++ strings.py
@@ -0,0 +1,9 @@
+my_string1 = 'mom'
+my_string2 = 'dad'
+sum1 = my_string1 + my_string2
+print(sum1)# -> momdad
+sum2 = 'my' + my_string1 + ' and my ' + my_string2
+print(sum2)# -> my mom and my dad
+#Operacje na poszczególnych elementach:
+print(sum2[3:6])# -> mom
+print(sum2[-4:-1])# -> dad
