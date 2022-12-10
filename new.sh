b61f03affb6a4f250a5a43f7ebaba92611fc7600
mateuszz_wsb <mateuszo.85@gmail.com>
2022-12-10 09:49:19 +0100

    new

---
+++ new.sh
@@ -0,0 +1,13 @@
+echo "# first_repo" >> README.md
+
+git init
+
+git add README.md
+
+git commit -m "first commit"
+
+git branch -M main
+
+git remote add origin https://github.com/Sailian1/first_repo.git
+
+git push -u origin main
